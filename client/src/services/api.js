e1a44cecaac1364ee6c12c3dc0ddd781da96de3e
saumya021 <saumya181711@gamil.com>
2026-05-29 22:07:18 +0530

    Fixed API base URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://personal-finance-tracker-z5bq.onrender.com",
+  baseURL: "https://personal-finance-tracker-z5bq.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
